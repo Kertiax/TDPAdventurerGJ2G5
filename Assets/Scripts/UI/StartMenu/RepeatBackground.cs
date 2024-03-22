0ce8c75cfc7225a940691c4c423cda1988218537
Jacobo <javasmu@gmail.com>
2024-03-22 13:27:46 -0500

    Background Art adjust

--- Assets/Scripts/UI/StartMenu/RepeatBackground.cs
+++ Assets/Scripts/UI/StartMenu/RepeatBackground.cs
@@ -6,15 +6,20 @@ public class RepeatBackground : MonoBehaviour
 {
     private Vector3 startPos;
     private float repeatWidth;
+    public GameObject playerPos;
 
     void Start()
     {
-        startPos = transform.position;
         repeatWidth = GetComponent<BoxCollider2D>().size.x / 2;
     }
 
     void Update()
     {
+        startPos = playerPos.transform.position;
+
+        transform.Translate(startPos,Space.Self);
+
+
         if (transform.position.x < startPos.x - repeatWidth){
             transform.position = startPos;
         }
